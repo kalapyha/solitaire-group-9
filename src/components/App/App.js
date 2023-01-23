9deb4d785a65352860cba8aace16b61e0a0e49c6
Nikolai Hapatnyukovsky <nikolaihapatnyukovsky@Nikolais-iMac.hitronhub.home>
2023-01-23 23:57:37 +0200

    chore: add eslint, ts and prettier

--- src/components/App/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-	return <div>Game will be here</div>;
-}
-
-export default App;
---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+    return <div>Game will be here</div>;
+}
+
+export default App;
