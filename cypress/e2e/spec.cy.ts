b0a685e2775846fcbb095f641e58ec41562438f0
Nick Khapatniukovskyi <86984401+Qlik-Nick@users.noreply.github.com>
2023-01-31 23:11:49 -0500

    asd

--- cypress/e2e/spec.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('empty spec', () => {
-    before(() => {
-        cy.visit('http://localhost:3000');
-    });
-
-    it('passes', () => {
-        cy.get('[data-testid="reset-score-btn"]');
-    });
-});
